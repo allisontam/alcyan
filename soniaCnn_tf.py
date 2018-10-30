5888aed907427dfaccd2f57f5c79db87b22bb581
Sanjana <sanjanas@mit.edu>
2018-10-30 10:44:57 -0400

    fixed spacing

--- soniaCnn_tf.py
+++ soniaCnn_tf.py
@@ -8,14 +8,17 @@ class SoniaLayer(keras.layers.Layer):
 
     def __init__(self, output_dim):
         super(SoniaLayer, self).__init__()
-        self.output_dim = output_dim 
-    
+        self.output_dim = output_dim
+ 
     def build(self, input_shape):
         self.kernel = self.add_variable("kernel", 
                                     shape=[input_shape[-1].value, 
                                            self.output_dim])
-    	
+
     def call(self, input):	# TODO my understanding is that this is the feed-forward part, so this is what a finished net would do to the input 
+        print('\n\nKERNEL SHAPE:', self.kernel.shape)
+        print('INPUT SHAPE:', tf.squeeze(input).shape, '\n\n')
+#         print('INPUT SHAPE:', tf.stack([tf.squeeze(input) for __ in range(self.output_dim)], axis=1).shape, '\n\n')
         A = self.kernel - tf.stack([tf.squeeze(input) for __ in range(self.output_dim)], axis=1)
         B = tf.square(A)
         C = tf.reduce_sum(B, axis=0)	# sum across all i, i.e. across the input dimension. WHY is that dimension 0 and not dimension 1???
@@ -23,7 +26,6 @@ class SoniaLayer(keras.layers.Layer):
         print('INPUT ENTRY TYPE:', type(int(input.shape[1])))
         return tf.reshape(tf.nn.tanh(D), [1, self.output_dim])	# TODO this ranges from [-1, 1] I think but in the paper it says it should be [0, 1] (page 76). What's up?. Also, tf.keras.layers.Layer doesn't automatically have an activation attribute (or support for the kwarg). Can I just stick it in so that this activation can be applied in a more tf-ish way, or should I leave it hardcoded? Will it be a part of the training this way?
 
-        
 
 num_classes = 10 
 
