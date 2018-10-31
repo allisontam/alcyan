fdc00034fb7d3cc831a2667b4834a9d1ce6d2fea
Allison Tam <actam@mit.edu>
2018-10-31 01:41:31 -0400

    torch simple cnn grayscale + dims replicated

--- simpleCnn_torch.py
+++ simpleCnn_torch.py
@@ -4,17 +4,18 @@ import torchvision
 import torchvision.transforms as transforms
 
 transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    [transforms.Grayscale(),
+     transforms.ToTensor(),
+     transforms.Normalize([0.5], [0.5])])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
                                           shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+testloader = torch.utils.data.DataLoader(testset, batch_size=1,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
@@ -27,20 +28,18 @@ import torch.nn.functional as F
 class Cnn(nn.Module):
     def __init__(self):
         super(Cnn, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv2 = nn.Conv2d(6, 10, 5)
+        self.fc1 = nn.Linear(10 * 5 * 5, 20) # input is flattened 10 5x5 filters
+        self.fc2 = nn.Linear(20, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 10 * 5 * 5)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 cnn = Cnn()
 
--- soniaCnn_tf.py
+++ soniaCnn_tf.py
@@ -9,13 +9,15 @@ class SoniaLayer(keras.layers.Layer):
     def __init__(self, output_dim):
         super(SoniaLayer, self).__init__()
         self.output_dim = output_dim
+        self.l_rate = 0.5
  
     def build(self, input_shape):
         self.kernel = self.add_variable("kernel", 
-                                    shape=[input_shape[-1].value, 
-                                           self.output_dim])
+                                    shape=[input_shape[-1].value,
+                                        self.output_dim])
 
     def call(self, input):	# TODO my understanding is that this is the feed-forward part, so this is what a finished net would do to the input 
+        # return tf.nn.relu(tf.matmul(input,self.kernel)) control for custom layer by trying Dense
         A = self.kernel - tf.stack([input for __ in range(self.output_dim)], axis=-1)
         B = tf.square(A)
         C = tf.reduce_sum(B, axis=1)	# sum across all i, i.e. across the input dimension. WHY is that dimension 0 and not dimension 1???
@@ -40,14 +42,17 @@ model.add(keras.layers.Flatten())
 # model.add(keras.layers.Dense(20)) # , activation='relu')
 # model.add(keras.layers.Activation('relu'))
 model.add(SoniaLayer(20))
+print model.output_shape, 'after sonia'
 model.add(keras.layers.Dense(num_classes)) # , activation='softmax')
 model.add(keras.layers.Activation('softmax'))
+print model.output_shape, 'after dense'
 
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+print tf.get_default_graph()
 model.fit(x_train, y_train, batch_size=32, validation_data=(x_test, y_test))
 
 scores = model.evaluate(x_test, y_test, verbose=1)
