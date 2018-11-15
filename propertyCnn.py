a7e55d78b44203cc91b77d8d7d4c7e374a9cda3f
Sanjana <sanjanas@mit.edu>
2018-11-15 00:23:40 -0500

    pool output

--- propertyCnn.py
+++ propertyCnn.py
@@ -13,7 +13,7 @@ transform = transforms.Compose([transforms.Grayscale(),
                                 transforms.ToTensor(), 
                                 transforms.Normalize([0.5], [0.5])])
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, num_workers=2)
 # trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, num_workers=2, sampler=torch.utils.data.SubsetRandomSampler(list(range(32))))
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
