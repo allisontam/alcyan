23429416f4e0ca5385227a8a8d69551e12f8a309
Allison Tam <actam@mit.edu>
2018-12-05 01:04:36 -0500

    batch norm for property cnn + separate activations for training/testing

--- propertyCnn.py
+++ propertyCnn.py
@@ -8,71 +8,84 @@ import torchvision.transforms as transforms
 
 
 # DATA
-num_classes = 10 
-transform = transforms.Compose([transforms.Grayscale(), 
-                                transforms.ToTensor(), 
+num_classes = 10
+transform = transforms.Compose([transforms.Grayscale(),
+                                transforms.ToTensor(),
                                 transforms.Normalize([0.5], [0.5])])
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=2)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, num_workers=2, sampler=torch.utils.data.SubsetRandomSampler(list(range(32))))
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=2) 
-# testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=2, sampler=torch.utils.data.SubsetRandomSampler(list(range(32))))
 
 # MODEL PARAMS
 L = 10**-2
 num_epochs = 5
+train = False
 
 
 # MODEL - adapted from https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
 class PropertyNet(nn.Module):
     def __init__(self):
         super(PropertyNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, 3)	# TODO should this be 3, 32, 3 bc 3-channel images?
-        self.conv2 = nn.Conv2d(8, 8, 3)
-        self.conv3 = nn.Conv2d(8, 16, 3)
-        self.conv4 = nn.Conv2d(16, 16, 3)
+        # TODO should this be 3, 32, 3 bc 3-channel images?
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 8, 3), \
+                nn.BatchNorm2d(8), \
+                nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Conv2d(8, 8, 3), \
+                nn.BatchNorm2d(8), \
+                nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Conv2d(8, 16, 3), \
+                nn.BatchNorm2d(16), \
+                nn.ReLU(inplace=True))
+        self.conv4 = nn.Sequential(nn.Conv2d(16, 16, 3), \
+                nn.BatchNorm2d(16), \
+                nn.ReLU(inplace=True))
+
         self.pool = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(400, num_classes)
         self.layers = nn.ModuleList([self.conv1, self.conv2, self.conv3, self.conv4, self.pool, self.fc1])
         self.pool_output = torch.randn(0, 400)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = self.conv1(x)
+        x = self.pool(self.conv2(x))
+        x = self.conv3(x)
+        x = self.pool(self.conv4(x))
         x = x.view(-1, 400)
+
+        # save activations into environ variable
         self.pool_output = torch.cat([self.pool_output, x], 0)
-#         torch.save(self.pool_output, 'pool_output.dat') 
         x = self.fc1(x)
         return x
 property_net = PropertyNet()
 
 # TRAINING
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(property_net.parameters(), lr=L)
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
-        optimizer.zero_grad()
-        outputs = property_net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+if train:
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(property_net.parameters(), lr=L)
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = property_net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        running_loss += loss.item()
-        if not i % 200:
-            print('[%d, %5d] loss: %.3f' % (epoch, i + 1, running_loss / 200))
-            running_loss = 0.0
+            running_loss += loss.item()
+            if not i % 200:
+                print('[%d, %5d] loss: %.3f' % (epoch, i + 1, running_loss / 200))
+                running_loss = 0.0
+    torch.save(property_net.state_dict(), "models/propCnn")
+property_net.load_state_dict( torch.load('models/propCnn') )
 
 property_net.pool_output = torch.randn(0, 400)
 print('Finished training')
+property_net.eval()
 
-
-# TESTING
+# SAVE TESTING ACTIVATIONS
 correct = 0
 total = 0
 all_outputs = np.array([])
@@ -80,17 +93,23 @@ with torch.no_grad():
     for data in testloader:
         images, labels = data
         outputs = property_net(images)
-#         print(property_net.layers[4].output.shape)
-        all_outputs = np.vstack([all_outputs, outputs]) if all_outputs.size else outputs        
+        all_outputs = np.vstack([all_outputs, outputs]) if all_outputs.size else outputs
         __, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-        i += 1
 
-# np.save('output_activations', all_outputs)
+np.save('output_activations', all_outputs)
+print(property_net.pool_output.shape, correct/total)
+torch.save(property_net.pool_output, 'test_activations.dat')
+
+# SAVE TRAINING ACTIVATIONS
+property_net.pool_output = torch.randn(0, 400)
+all_outputs = np.array([])
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=False, num_workers=2)
+with torch.no_grad():
+    for data in trainloader:
+        images, labels = data
+        outputs = property_net(images)
+
 print(property_net.pool_output.shape)
-torch.save(property_net.pool_output, 'all_pool_output.dat')
-
-print('Accuracy of network on 10000 test images: %d %%' % (100 * correct / total))
-
-
+torch.save(property_net.pool_output, 'train_activations.dat')
